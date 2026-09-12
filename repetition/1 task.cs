8fd2baf5ccad662370a072329db597dc9b7661c8
dezue <mistfoll@mail.ru>
2026-09-12 07:36:49 +0700

    1 task

---
+++ repetition/1 task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GeometryTask
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Enter the cube edge: ");
+      double cubeEdge = Convert.ToDouble(Console.ReadLine());
+
+      Console.Write("Enter the ball radius: ");
+      double ballRadius = Convert.ToDouble(Console.ReadLine());
+
+      double sphereVolumeFactor = 4.0 / 3.0;
+      double percentMultiplier = 100.0;
+
+      double cubeVolume = cubeEdge * cubeEdge * cubeEdge;
+
+      double ballVolume = sphereVolumeFactor * Math.PI * ballRadius * ballRadius * ballRadius;
+
+      double wastePercent = ((cubeVolume - ballVolume) / cubeVolume) * percentMultiplier;
+
+      Console.WriteLine("Cube volume: " + cubeVolume);
+      Console.WriteLine("Ball volume: " + ballVolume);
+      Console.WriteLine("Waste percent: " + wastePercent);
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ repetition/2 task.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace repetition
+{
+  internal class _2_task
+  {
+  }
+}
